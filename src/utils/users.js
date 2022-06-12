ece46f3c39064118603074bb18a41a716016a355
Qmun14 <mamunramdhan@gmail.com>
2022-06-12 10:30:36 +0700

    Menyelesaikan Materi (Storing USers Part.2) dengan membuat, menguuji dan meng-exportnya beberapa function di file users.js di folder utils, untuk keperluan kedepannya

--- src/utils/users.js
+++ src/utils/users.js
@@ -40,15 +40,18 @@ const removeUser = (id) => {
     }
 }
 
- addUser({
-    id : 22,
-    username : 'Qmun14    ',
-    room : '   Bogor'
-})
+const getUser = (id) => {
+    return users.find((user) => user.id === id)
+    
+}
 
-console.log(users);
+const getUsersInRoom = (room) => {
+    return users.filter((user) => user.room === room)
+}
 
-const removedUser = removeUser(22)
-
-console.log(removedUser)
-console.log(users)
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+}
